abc330111bdad8fb4a1675871bbbced2634e6889
ryaalbr <ryaalbr@nuevaschool.org>
2025-07-27 15:38:33 -0700

    Implemented Ch. 4

--- prompt.c
+++ prompt.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+/* if compiling on windows, compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* fake readline func */
+
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer) + 1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
+
+/* fake addhist func */
+void add_history(char* unused) {}
+
+/* otherwise include editline headers */ 
+#else
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
 
@@ -13,13 +36,16 @@ int main(int argc, char** argv) {
 	while (1) {
 
 		/* output prompt */
-		fputs("lispy> ", stdout);
+		char* input = readline("lispy> ");
 		
-		/* read line from user input, max size 2048 */
-		fgets(input, 2048, stdin);
-
+		/* add input to history */
+		add_history(input);
+		
 		/* echo input back to user */
-		printf("No you're a %s", input);
+		printf("No you're a %s\n", input);
+
+		/* free retrieved input */
+		free(input);
 	}
 
 	return 0;
